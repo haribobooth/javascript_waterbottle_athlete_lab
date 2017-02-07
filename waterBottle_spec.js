95fda9c28685ed45f5823df3b8a9cce3638607d1
Harrison Booth <harrisonbooth@ymail.com>
2017-02-07 15:01:00 +0000

    rename wB to waterBottle

--- waterBottle_spec.js
+++ waterBottle_spec.js
@@ -1,29 +1,28 @@
 var assert = require('assert');
-
-var wB = require('./waterBottle');
+var waterBottle = require('./waterBottle');
 
 describe('waterBottle', function(){
   it('should start empty', function(){
-    assert.equal(0, wB.amount);
+    assert.equal(0, waterBottle.amount);
   });
   it('should get full', function(){
-    wB.fill();
-    assert.equal(100, wB.amount);
+    waterBottle.fill();
+    assert.equal(100, waterBottle.amount);
   });
   it('when drunk down by 10?', function(){
-    wB.fill();
-    assert.equal(10, wB.drink());
-    assert.equal(90, wB.amount);
+    waterBottle.fill();
+    assert.equal(10, waterBottle.drink());
+    assert.equal(90, waterBottle.amount);
   });
   it('should be able to empty', function(){
-    wB.fill();
-    wB.empty();
-    assert.equal(0, wB.amount);
+    waterBottle.fill();
+    waterBottle.empty();
+    assert.equal(0, waterBottle.amount);
   });
   it('no negative amount', function(){
-    wB.empty();
-    assert.equal("No water", wB.drink());
-    
-    assert.equal(0, wB.amount);
+    waterBottle.empty();
+    assert.equal("No water", waterBottle.drink());
+
+    assert.equal(0, waterBottle.amount);
   });
 });
